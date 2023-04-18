bbf2b3d7f1fdffa347bfe985c545a24d13b7b366
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-04-18 14:10:27 +0300

    Add project files.

---
+++ Prac18_04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prac18_04
+{
+    internal class Program
+    {
+        public static void ToStringThread(object obj)
+        {
+
+            List<int> list = new List<int>((List<int>)obj);
+            foreach (var i in list)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+            for(int i=0; i<10; i++)
+                list.Add(rnd.Next(0,100));
+            Thread th = new Thread(new ParameterizedThreadStart(ToStringThread));
+            th.Start(list);
+            Console.Read();
+        }
+    }
+}
